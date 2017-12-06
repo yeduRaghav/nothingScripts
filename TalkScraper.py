371c5e56293e56e693eb588b899c7c3617c1eac7
rgv <mailyeduraghav@gmail.com>
2017-12-06 15:57:36 -0500

    removed comments

--- TalkScraper.py
+++ TalkScraper.py
@@ -11,10 +11,6 @@ for path in soup.find_all('a'):
     if path.get('href').find('.mp3') != -1:
         paths.append(path.get('href'))
 
-
-#downloadDirectory =
-
-
 for index, path in enumerate(paths):
     fileUrl = domain+path
     response = requests.get(fileUrl)
@@ -22,9 +18,3 @@ for index, path in enumerate(paths):
         blobFile.write(response.content)
     print("Downloaded "+str(index)+"/"+str(len(paths))+" : "+ path)
     sleep(10)
-
-
-#fileUrl = domain+paths[0]
-#audioFile = requests.get(fileUrl)
-#with open(r'/Users/yeduraghav/Desktop/nothingtalks/'+paths[0], 'wb') as ff:
-#    ff.write(audioFile.content)
